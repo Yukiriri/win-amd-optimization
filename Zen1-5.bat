c06fbb2f10cee370ba03848da1a815802fe92b5d
雪リリ <yukinshell@qq.com>
2024-08-11 04:04:55 +0800

    小优化

--- Zen1-5.bat
+++ Zen1-5.bat
@@ -8,8 +8,6 @@ for %%i in (SCHEME_BALANCED SCHEME_MIN e9a42b02-d5df-448d-aa00-03f14749eb61) do 
     echo %%i 异类线程调度策略=所有处理器
     powercfg -setacvalueindex %%i SUB_PROCESSOR SHORTSCHEDPOLICY 0
     echo %%i 异类短运行线程调度策略=所有处理器
-    powercfg -setacvalueindex %%i SUB_PROCESSOR SMTUNPARKPOLICY 2
-    echo %%i SMT线程启动策略=循环配置
     echo.
 )
 powercfg -setacvalueindex SCHEME_BALANCED SUB_PROCESSOR CPMINCORES 50
@@ -20,6 +18,8 @@ powercfg -setacvalueindex SCHEME_BALANCED SUB_PROCESSOR CPDECREASEPOL 1
 echo SCHEME_BALANCED 处理器性能核心放置减小策略=单一核心
 powercfg -setacvalueindex SCHEME_BALANCED SUB_PROCESSOR CPINCREASEPOL 1
 echo SCHEME_BALANCED 处理器性能核心放置增加策略=单一核心
+powercfg -setacvalueindex SCHEME_BALANCED SUB_PROCESSOR SMTUNPARKPOLICY 2
+echo SCHEME_BALANCED SMT线程启动策略=循环配置
 echo.
 
 reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power /v EventProcessorEnabled /t REG_DWORD /d 0 /f
