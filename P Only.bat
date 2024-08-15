9239b9f5f19c705ca167a70d8476d0f79cb39b4e
雪リリ <yukinshell@qq.com>
2024-08-15 15:10:22 +0800

    改一下阅读顺序

--- P Only.bat
+++ P Only.bat
@@ -2,13 +2,15 @@
 
 for %%i in (SCHEME_BALANCED SCHEME_MIN e9a42b02-d5df-448d-aa00-03f14749eb61) do (
     powercfg -setacvalueindex %%i SUB_PROCESSOR HETEROPOLICY 4
-    echo [%%i][生效的异类策略]=使用异类策略4
-    echo [%%i][Heterogeneous policy in effect]=Use heterogeneous policy 4
     powercfg -setacvalueindex %%i SUB_PROCESSOR SCHEDPOLICY 0
-    echo [%%i][异类线程调度策略]=所有处理器
-    echo [%%i][Heterogeneous thread scheduling policy]=All processors
     powercfg -setacvalueindex %%i SUB_PROCESSOR SHORTSCHEDPOLICY 0
+
+    echo [%%i][生效的异类策略]=使用异类策略4
+    echo [%%i][异类线程调度策略]=所有处理器
     echo [%%i][异类短运行线程调度策略]=所有处理器
+    
+    echo [%%i][Heterogeneous policy in effect]=Use heterogeneous policy 4
+    echo [%%i][Heterogeneous thread scheduling policy]=All processors
     echo [%%i][Heterogeneous short running thread scheduling policy]=All processors
     echo.
 )
--- P+E.bat
+++ P+E.bat
@@ -2,13 +2,15 @@
 
 for %%i in (SCHEME_BALANCED SCHEME_MIN e9a42b02-d5df-448d-aa00-03f14749eb61) do (
     powercfg -setacvalueindex %%i SUB_PROCESSOR HETEROPOLICY 0
-    echo [%%i][生效的异类策略]=使用异类策略0
-    echo [%%i][Heterogeneous policy in effect]=Use heterogeneous policy 0
     powercfg -setacvalueindex %%i SUB_PROCESSOR SCHEDPOLICY 2
-    echo [%%i][异类线程调度策略]=首选高性能处理器
-    echo [%%i][Heterogeneous thread scheduling policy]=Prefer performant processors
     powercfg -setacvalueindex %%i SUB_PROCESSOR SHORTSCHEDPOLICY 2
+
+    echo [%%i][生效的异类策略]=使用异类策略0
+    echo [%%i][异类线程调度策略]=首选高性能处理器
     echo [%%i][异类短运行线程调度策略]=首选高性能处理器
+    
+    echo [%%i][Heterogeneous policy in effect]=Use heterogeneous policy 0
+    echo [%%i][Heterogeneous thread scheduling policy]=Prefer performant processors
     echo [%%i][Heterogeneous short running thread scheduling policy]=Prefer performant processors
     echo.
 )
