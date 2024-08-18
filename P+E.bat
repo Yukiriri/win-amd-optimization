b2bd6a76e22e7276dcde3059c7ca1c64db0aa2ad
雪リリ <yukinshell@qq.com>
2024-08-18 13:11:33 +0800

    修复不立即生效问题

--- P+E.bat
+++ P+E.bat
@@ -4,18 +4,20 @@ for %%i in (SCHEME_BALANCED SCHEME_MIN e9a42b02-d5df-448d-aa00-03f14749eb61) do 
     powercfg -setacvalueindex %%i SUB_PROCESSOR HETEROPOLICY 0
     powercfg -setacvalueindex %%i SUB_PROCESSOR SCHEDPOLICY 2
     powercfg -setacvalueindex %%i SUB_PROCESSOR SHORTSCHEDPOLICY 2
+    powercfg -setacvalueindex %%i SUB_PROCESSOR SMTUNPARKPOLICY 2
 
     echo [%%i][生效的异类策略]=使用异类策略0
     echo [%%i][异类线程调度策略]=首选高性能处理器
     echo [%%i][异类短运行线程调度策略]=首选高性能处理器
+    echo [%%i][SMT线程启动策略]=循环配置
     
     echo [%%i][Heterogeneous policy in effect]=Use heterogeneous policy 0
     echo [%%i][Heterogeneous thread scheduling policy]=Prefer performant processors
     echo [%%i][Heterogeneous short running thread scheduling policy]=Prefer performant processors
+    echo [%%i][Smt threads unpark policy]=Round robin
     echo.
 )
 
-@REM reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power /v EventProcessorEnabled /t REG_DWORD /d 0 /f
-@REM bcdedit /deletevalue useplatformclock >nul
+powercfg -S SCHEME_CURRENT
 
 pause
--- SMT Hibernate On.bat
+++ SMT Hibernate On.bat
@@ -1,13 +1,12 @@
 @echo off & chcp 65001 >nul
 
 for %%i in (SCHEME_BALANCED SCHEME_MIN e9a42b02-d5df-448d-aa00-03f14749eb61) do (
-    powercfg -setacvalueindex SCHEME_BALANCED SUB_PROCESSOR CPMINCORES 50
-    @REM powercfg -setacvalueindex SCHEME_BALANCED SUB_PROCESSOR CPMINCORES1 50
-    powercfg -setacvalueindex SCHEME_BALANCED SUB_PROCESSOR CPHEADROOM 98
-    powercfg -setacvalueindex SCHEME_BALANCED SUB_PROCESSOR CPDECREASEPOL 1
-    powercfg -setacvalueindex SCHEME_BALANCED SUB_PROCESSOR CPINCREASEPOL 1
-    powercfg -setacvalueindex SCHEME_BALANCED SUB_PROCESSOR PERFCHECK 30
-    powercfg -setacvalueindex SCHEME_BALANCED SUB_PROCESSOR SMTUNPARKPOLICY 2
+    powercfg -setacvalueindex %%i SUB_PROCESSOR CPMINCORES 50
+    @REM powercfg -setacvalueindex %%i SUB_PROCESSOR CPMINCORES1 50
+    powercfg -setacvalueindex %%i SUB_PROCESSOR CPHEADROOM 98
+    powercfg -setacvalueindex %%i SUB_PROCESSOR CPDECREASEPOL 1
+    powercfg -setacvalueindex %%i SUB_PROCESSOR CPINCREASEPOL 1
+    powercfg -setacvalueindex %%i SUB_PROCESSOR PERFCHECK 30
 
     echo [%%i][处理器性能核心放置最小核心数量]=50%%
     @REM echo [%%i][针对第1类处理器电源效率的处理器性能核心放置最小核心数量]=50%%
@@ -15,7 +14,6 @@ for %%i in (SCHEME_BALANCED SCHEME_MIN e9a42b02-d5df-448d-aa00-03f14749eb61) do 
     echo [%%i][处理器性能核心放置减小策略]=单一核心
     echo [%%i][处理器性能核心放置增加策略]=单一核心
     echo [%%i][处理器性能时间检查间隔]=30ms
-    echo [%%i][SMT线程启动策略]=循环配置
     
     echo [%%i][Processor performance core parking min cores]=50%%
     @REM echo [%%i][Processor performance core parking min cores for Processor Power Efficiency Class 1]=50%%
@@ -23,8 +21,9 @@ for %%i in (SCHEME_BALANCED SCHEME_MIN e9a42b02-d5df-448d-aa00-03f14749eb61) do 
     echo [%%i][Processor performance core parking decrease policy]=Single core
     echo [%%i][Processor performance core parking increase policy]=Single core
     echo [%%i][Processor performance time check interval]=30ms
-    echo [%%i][Smt threads unpark policy]=Round robin
     echo.
 )
+
+powercfg -S SCHEME_CURRENT
 
 pause
